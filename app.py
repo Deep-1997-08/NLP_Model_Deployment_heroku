1ecc41e16df792dfc0d8e2dd661543fed0391d34
Deep Bhattacharjee <51170317+Deep-1997-08@users.noreply.github.com>
2024-07-13 14:53:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ def home():
 @app.route('/predict', method=['POST'])
 def predict():
   if request.method == 'POST':
-		message = request.form['message']
-		data = [message]
-		vect = cv.transform(data).toarray()
-		my_prediction = clf.predict(vect)
-	return render_template('result.html',prediction = my_prediction)
+    message = request.form['message']
+    data = [message]
+    vect = cv.transform(data).toarray()
+    my_prediction = clf.predict(vect)
+  return render_template('result.html', prediction = my_prediction)
 
 if __name__ == '__main__':
   app.run(debug=True)
